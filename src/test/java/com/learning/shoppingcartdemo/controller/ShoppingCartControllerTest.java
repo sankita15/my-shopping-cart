1fdea90c47fcb273edb119696fa15f3eececbf92
Ankita Srivastava <ankitacs0018@gmail.com>
2019-06-01 14:52:32 +0530

    Add Cart controller and Cart service test for add, read, readById, readByUsername

---
+++ src/test/java/com/learning/shoppingcartdemo/controller/ShoppingCartControllerTest.java
@@ -0,0 +1,154 @@
+package com.learning.shoppingcartdemo.controller;
+
+import com.learning.shoppingcartdemo.config.JwtUtilConfig;
+import com.learning.shoppingcartdemo.config.TestConfig;
+import com.learning.shoppingcartdemo.model.Product;
+import com.learning.shoppingcartdemo.model.ShoppingCart;
+import com.learning.shoppingcartdemo.service.CartService;
+import lombok.SneakyThrows;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureWebTestClient
+@SpringBootTest(classes = {TestConfig.class, JwtUtilConfig.class})
+public class ShoppingCartControllerTest {
+
+    @MockBean
+    CartService cartService;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private static final String IMAGE_URL1 = "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png";
+    private static final String MOCK_BEARER_AUTH_TOKEN = "Bearer 1234567f.gh.jklgvbhcj8s";
+
+    private static final SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    private static final Product product1 = Product.builder().id("1")
+        .productName("Leaf Rake")
+        .productCode("GDN-0011")
+        .price(1995)
+        .releaseDate(getParsedDate("2017-03-19T15:15:55.570Z"))
+        .description("Leaf rake with 48-inch wooden handle.")
+        .starRating(3.2f)
+        .imageUrl(IMAGE_URL1)
+        .stock(400).build();
+
+    @SneakyThrows
+    private static Date getParsedDate(String dateString) {
+        return date.parse(dateString);
+    }
+
+    private static final ShoppingCart CART1 = ShoppingCart.builder()
+        .id("1")
+        .username("user")
+        .orderDate(new Date())
+        .cartStatus(ShoppingCart.PENDING)
+        .products(getProducts("1", product1))
+        .productQuantities(getProductQuantity("1", 5))
+        .cartPrice(400)
+        .build();
+
+    private static final ShoppingCart CART2 = ShoppingCart.builder()
+        .id("2")
+        .username("alice")
+        .orderDate(new Date())
+        .cartStatus(ShoppingCart.PENDING)
+        .products(getProducts("2", product1))
+        .productQuantities(getProductQuantity("2", 2))
+        .cartPrice(400)
+        .build();
+
+    private static HashMap<String, Integer> getProductQuantity(String id, Integer quantity) {
+        HashMap productQuantities = new HashMap<>();
+        productQuantities.put(id, quantity);
+        return productQuantities;
+    }
+
+    private static HashMap<String, Product> getProducts(String id , Product product1) {
+        HashMap<String, Product> product = new HashMap<>();
+        product.put(id, product1);
+        return product;
+    }
+
+    @Test
+    public void shouldReturnAllTheCarts() {
+
+        when(cartService.getAllCarts()).thenReturn(Flux.just(CART1, CART2));
+
+        webTestClient.get().uri("/api/carts")
+            .header("Authorization", MOCK_BEARER_AUTH_TOKEN)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(ShoppingCart.class)
+            .hasSize(2)
+            .contains(CART1, CART2);
+
+        verify(cartService, times(1)).getAllCarts();
+    }
+
+    @Test
+    public void shouldAddCart() {
+        when(cartService.addCart(CART1)).thenReturn(Mono.just(CART1));
+
+        webTestClient.post().uri("/api/carts/add")
+            .body(Mono.just(CART1), ShoppingCart.class)
+            .header("AUTHORIZATION", MOCK_BEARER_AUTH_TOKEN)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(ShoppingCart.class)
+            .isEqualTo(CART1);
+
+        verify(cartService, times(1)).addCart(CART1);
+    }
+
+    @Test
+    public void shouldReturnCartByCartID() {
+        when(cartService.getCartByCartId("1")).thenReturn(Mono.just(CART1));
+
+        webTestClient.get().uri("/api/carts/1")
+            .header("AUTHORIZATION", MOCK_BEARER_AUTH_TOKEN)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(ShoppingCart.class)
+            .isEqualTo(CART1);
+
+        verify(cartService, times(1)).getCartByCartId("1");
+    }
+
+    @Test
+    public void shouldReturnCartByUsername() {
+        when(cartService.getCartByUsername("alice")).thenReturn(Flux.just(CART2));
+
+        webTestClient.get().uri("/api/carts/user/alice")
+            .header("AUTHORIZATION", MOCK_BEARER_AUTH_TOKEN)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(ShoppingCart.class)
+            .hasSize(1)
+            .contains(CART2);
+
+        verify(cartService, times(1)).getCartByUsername("alice");
+    }
+}
---
+++ src/test/java/com/learning/shoppingcartdemo/service/CartServiceTest.java
@@ -0,0 +1,121 @@
+package com.learning.shoppingcartdemo.service;
+
+import com.learning.shoppingcartdemo.model.Product;
+import com.learning.shoppingcartdemo.model.ShoppingCart;
+import com.learning.shoppingcartdemo.repository.CartRepository;
+import lombok.SneakyThrows;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class CartServiceTest {
+
+    @Mock
+    private CartRepository cartRepository;
+
+    private CartService cartService;
+
+    @Before
+    public void setUp() throws Exception {
+        cartService = new CartService(cartRepository);
+    }
+
+    private static final String IMAGE_URL1 = "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png";
+
+    private static final SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    private static final Product product1 = Product.builder().id("1")
+        .productName("Leaf Rake")
+        .productCode("GDN-0011")
+        .price(1995)
+        .releaseDate(getParsedDate("2017-03-19T15:15:55.570Z"))
+        .description("Leaf rake with 48-inch wooden handle.")
+        .starRating(3.2f)
+        .imageUrl(IMAGE_URL1)
+        .stock(400).build();
+
+    @SneakyThrows
+    private static Date getParsedDate(String dateString) {
+        return date.parse(dateString);
+    }
+
+    private static final ShoppingCart CART1 = ShoppingCart.builder()
+        .id("1")
+        .username("user")
+        .orderDate(new Date())
+        .cartStatus(ShoppingCart.PENDING)
+        .products(getProducts("1", product1))
+        .productQuantities(getProductQuantity("1", 5))
+        .cartPrice(400)
+        .build();
+
+    private static HashMap<String, Integer> getProductQuantity(String id, Integer quantity) {
+        HashMap productQuantities = new HashMap<>();
+        productQuantities.put(id, quantity);
+        return productQuantities;
+    }
+
+    private static HashMap<String, Product> getProducts(String id , Product product1) {
+        HashMap<String, Product> product = new HashMap<>();
+        product.put(id, product1);
+        return product;
+    }
+
+    @Test
+    public void shouldReturnListOfAllTheCart() {
+        when(cartRepository.findAll()).thenReturn(Flux.just(CART1));
+
+        StepVerifier.create(cartService.getAllCarts())
+            .expectNext(CART1)
+            .verifyComplete();
+
+        verify(cartRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void shouldAddCart() {
+        when(cartRepository.insert(CART1)).thenReturn(Mono.just(CART1));
+
+        StepVerifier.create(cartService.addCart(CART1))
+            .expectNext(CART1)
+            .verifyComplete();
+
+        verify(cartRepository, times(1)).insert(CART1);
+    }
+
+    @Test
+    public void shouldReturnCartByCartId() {
+        when(cartRepository.findById("1")).thenReturn(Mono.just(CART1));
+
+        StepVerifier.create(cartService.getCartByCartId("1"))
+            .expectNext(CART1)
+            .verifyComplete();
+
+        verify(cartRepository, times(1)).findById("1");
+    }
+
+    @Test
+    public void shouldReturnCartByUsername() {
+        when(cartRepository.findByUsername("user")).thenReturn(Flux.just(CART1));
+
+        StepVerifier.create(cartService.getCartByUsername("user"))
+            .expectNext(CART1)
+            .verifyComplete();
+
+        verify(cartRepository, times(1)).findByUsername("user");
+    }
+}
